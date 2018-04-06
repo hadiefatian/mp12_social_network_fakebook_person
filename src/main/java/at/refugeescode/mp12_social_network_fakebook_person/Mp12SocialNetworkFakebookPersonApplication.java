a16513249cb031197a6428acbcfec447330ce8d0
Hadi <hadi.effatian@gmail.com>
2018-04-06 14:57:35 +0200

    Created Project

---
+++ src/main/java/at/refugeescode/mp12_social_network_fakebook_person/Mp12SocialNetworkFakebookPersonApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp12_social_network_fakebook_person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp12SocialNetworkFakebookPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp12SocialNetworkFakebookPersonApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp12_social_network_fakebook_person/Mp12SocialNetworkFakebookPersonApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp12_social_network_fakebook_person;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp12SocialNetworkFakebookPersonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
